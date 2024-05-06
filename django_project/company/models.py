93d4a362b3d20f363716e6c9d69f0760f2ffdcb3
Muhammed Nael <imohmad7@gmail.com>
2024-05-07 02:58:29 +0300

    added template

--- django_project/company/models.py
+++ django_project/company/models.py
@@ -5,13 +5,14 @@ from users.models import User
 
 class Company(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    est_date = models.PositiveIntegerField()   
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    description = models.TextField()
-    logo = models.ImageField(upload_to='company_logos/', default='company_logos/default.jpg')
-    website = models.URLField(max_length=200)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    est_date = models.PositiveIntegerField(null=True, blank=True)   
+    city = models.CharField(max_length=100, null=True, blank=True)
+    state = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    logo = models.ImageField(upload_to='company_logos/', default='company_logos/default.jpg', null=True, blank=True)
+    website = models.URLField(max_length=200, null=True, blank=True)
     # company size
     
-    
+    def __str__(self):
+        return self.name
--- django_project/resume/models.py
+++ django_project/resume/models.py
@@ -4,8 +4,11 @@ from users.models import User
 
 class Resume(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    job_title = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    job_title = models.CharField(max_length=100, null=True, blank=True)
     # insert cv and parse it using pdf2txt ?
+    
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
