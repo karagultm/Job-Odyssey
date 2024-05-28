c69fceca09ccee27c761199f72daf48fe28b4271
kadirbat <kadirbat@marun.edu.tr>
2024-05-28 14:27:38 +0300

    edit profile and myprofile finished

--- app/src/components/EditProfile.js
+++ app/src/components/EditProfile.js
@@ -204,6 +204,11 @@ const MyProfile = ({ refreshNavbar }) => {
       toast.error('No user is logged in');
     }
   }
+  const handleRemoveItem = (list, setList, index) => {
+    const newList = [...list];
+    newList.splice(index, 1);
+    setList(newList);
+  };
 
   const handleSkillsChange = (selectedOptions) => {
     setPersonalInfo({ ...personalInfo, skills: selectedOptions.map(option => option.value) });
@@ -335,6 +340,224 @@ const MyProfile = ({ refreshNavbar }) => {
           School Internship Match
           <input type="checkbox" name="schoolInternshipMatch" checked={personalInfo.schoolInternshipMatch} onChange={handleInputChange} />
         </label>
+        <h3>Educational Information</h3>
+        <button type="button" onClick={() => setEducation([...education, { university: '', startDate: '', endDate: '', gpa: '' }])}>Add Education</button>
+        {education.map((edu, index) => (
+          <div key={index}>
+            <label>
+              University
+              <input type="text" value={edu.university} onChange={(e) => {
+                const newEducation = [...education];
+                newEducation[index].university = e.target.value;
+                setEducation(newEducation);
+              }} required />
+            </label>
+            <label>
+              Start Date
+              <input type="date" value={edu.startDate} onChange={(e) => {
+                const newEducation = [...education];
+                newEducation[index].startDate = e.target.value;
+                setEducation(newEducation);
+              }} required />
+            </label>
+            <label>
+              End Date
+              <input type="date" value={edu.endDate} onChange={(e) => {
+                const newEducation = [...education];
+                newEducation[index].endDate = e.target.value;
+                setEducation(newEducation);
+              }} required />
+            </label>
+            <label>
+              GPA
+              <input type="number" value={edu.gpa} onChange={(e) => {
+                const newEducation = [...education];
+                newEducation[index].gpa = e.target.value;
+                setEducation(newEducation);
+              }} required />
+            </label>
+            <button type="button" onClick={() => handleRemoveItem(education, setEducation, index)}>Remove</button>
+          </div>
+        ))}
+        <h3>Work Experience</h3>
+        <button type="button" onClick={() => setWorkExperience([...workExperience, { companyName: '', jobTitle: '', startDate: '', endDate: '', jobDescription: '' }])}>Add Work Experience</button>
+        {workExperience.map((work, index) => (
+          <div key={index}>
+            <label>
+              Company Name
+              <input type="text" value={work.companyName} onChange={(e) => {
+                const newWorkExperience = [...workExperience];
+                newWorkExperience[index].companyName = e.target.value;
+                setWorkExperience(newWorkExperience);
+              }} required />
+            </label>
+            <label>
+              Job Title
+              <input type="text" value={work.jobTitle} onChange={(e) => {
+                const newWorkExperience = [...workExperience];
+                newWorkExperience[index].jobTitle = e.target.value;
+                setWorkExperience(newWorkExperience);
+              }} required />
+            </label>
+            <label>
+              Start Date
+              <input type="date" value={work.startDate} onChange={(e) => {
+                const newWorkExperience = [...workExperience];
+                newWorkExperience[index].startDate = e.target.value;
+                setWorkExperience(newWorkExperience);
+              }} required />
+            </label>
+            <label>
+              End Date
+              <input type="date" value={work.endDate} onChange={(e) => {
+                const newWorkExperience = [...workExperience];
+                newWorkExperience[index].endDate = e.target.value;
+                setWorkExperience(newWorkExperience);
+              }} required />
+            </label>
+            <label>
+              Job Description
+              <textarea value={work.jobDescription} onChange={(e) => {
+                const newWorkExperience = [...workExperience];
+                newWorkExperience[index].jobDescription = e.target.value;
+                setWorkExperience(newWorkExperience);
+              }} required />
+            </label>
+            <button type="button" onClick={() => handleRemoveItem(workExperience, setWorkExperience, index)}>Remove</button>
+          </div>
+        ))}
+        <h3>Volunteer Experience</h3>
+        <button type="button" onClick={() => setVolunteerExperience([...volunteerExperience, { organization: '', role: '', startDate: '', endDate: '', description: '' }])}>Add Volunteer Experience</button>
+        {volunteerExperience.map((volunteer, index) => (
+          <div key={index}>
+            <label>
+              Organization
+              <input type="text" value={volunteer.organization} onChange={(e) => {
+                const newVolunteerExperience = [...volunteerExperience];
+                newVolunteerExperience[index].organization = e.target.value;
+                setVolunteerExperience(newVolunteerExperience);
+              }} required />
+            </label>
+            <label>
+              Role
+              <input type="text" value={volunteer.role} onChange={(e) => {
+                const newVolunteerExperience = [...volunteerExperience];
+                newVolunteerExperience[index].role = e.target.value;
+                setVolunteerExperience(newVolunteerExperience);
+              }} required />
+            </label>
+            <label>
+              Start Date
+              <input type="date" value={volunteer.startDate} onChange={(e) => {
+                const newVolunteerExperience = [...volunteerExperience];
+                newVolunteerExperience[index].startDate = e.target.value;
+                setVolunteerExperience(newVolunteerExperience);
+              }} required />
+            </label>
+            <label>
+              End Date
+              <input type="date" value={volunteer.endDate} onChange={(e) => {
+                const newVolunteerExperience = [...volunteerExperience];
+                newVolunteerExperience[index].endDate = e.target.value;
+                setVolunteerExperience(newVolunteerExperience);
+              }} required />
+            </label>
+            <label>
+              Description
+              <textarea value={volunteer.description} onChange={(e) => {
+                const newVolunteerExperience = [...volunteerExperience];
+                newVolunteerExperience[index].description = e.target.value;
+                setVolunteerExperience(newVolunteerExperience);
+              }} required />
+            </label>
+            <button type="button" onClick={() => handleRemoveItem(volunteerExperience, setVolunteerExperience, index)}>Remove</button>
+          </div>
+        ))}
+        <h3>Languages</h3>
+        <button type="button" onClick={() => setLanguages([...languages, { language: '', proficiency: '' }])}>Add Language</button>
+        {languages.map((lang, index) => (
+          <div key={index}>
+            <label>
+              Language
+              <input type="text" value={lang.language} onChange={(e) => {
+                const newLanguages = [...languages];
+                newLanguages[index].language = e.target.value;
+                setLanguages(newLanguages);
+              }} required />
+            </label>
+            <label>
+              Proficiency
+              <input type="text" value={lang.proficiency} onChange={(e) => {
+                const newLanguages = [...languages];
+                newLanguages[index].proficiency = e.target.value;
+                setLanguages(newLanguages);
+              }} required />
+            </label>
+            <button type="button" onClick={() => handleRemoveItem(languages, setLanguages, index)}>Remove</button>
+          </div>
+        ))}
+        <h3>Courses</h3>
+        <button type="button" onClick={() => setCourses([...courses, { courseName: '', institution: '', completionDate: '' }])}>Add Course</button>
+        {courses.map((course, index) => (
+          <div key={index}>
+            <label>
+              Course Name
+              <input type="text" value={course.courseName} onChange={(e) => {
+                const newCourses = [...courses];
+                newCourses[index].courseName = e.target.value;
+                setCourses(newCourses);
+              }} />
+            </label>
+            <label>
+              Institution
+              <input type="text" value={course.institution} onChange={(e) => {
+                const newCourses = [...courses];
+                newCourses[index].institution = e.target.value;
+                setCourses(newCourses);
+              }} />
+            </label>
+            <label>
+              Completion Date
+              <input type="date" value={course.completionDate} onChange={(e) => {
+                const newCourses = [...courses];
+                newCourses[index].completionDate = e.target.value;
+                setCourses(newCourses);
+              }} />
+            </label>
+            <button type="button" onClick={() => handleRemoveItem(courses, setCourses, index)}>Remove</button>
+          </div>
+        ))}
+        <h3>Certifications</h3>
+        <button type="button" onClick={() => setCertifications([...certifications, { certificationName: '', institution: '', issuanceDate: '' }])}>Add Certification</button>
+        {certifications.map((certification, index) => (
+          <div key={index}>
+            <label>
+              Certification Name
+              <input type="text" value={certification.certificationName} onChange={(e) => {
+                const newCertifications = [...certifications];
+                newCertifications[index].certificationName = e.target.value;
+                setCertifications(newCertifications);
+              }} />
+            </label>
+            <label>
+              Institution
+              <input type="text" value={certification.institution} onChange={(e) => {
+                const newCertifications = [...certifications];
+                newCertifications[index].institution = e.target.value;
+                setCertifications(newCertifications);
+              }} />
+            </label>
+            <label>
+              Issuance Date
+              <input type="date" value={certification.issuanceDate} onChange={(e) => {
+                const newCertifications = [...certifications];
+                newCertifications[index].issuanceDate = e.target.value;
+                setCertifications(newCertifications);
+              }} />
+            </label>
+            <button type="button" onClick={() => handleRemoveItem(certifications, setCertifications, index)}>Remove</button>
+          </div>
+        ))}
         <button type="submit">Save Profile</button>
       </form>
     </div>
