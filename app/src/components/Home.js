98cee0d1f3f52cbf6d8bcda5be52d89244d23bf3
Muhammed Nael <imohmad7@gmail.com>
2024-05-29 10:40:32 +0300

    fixed searching func

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import '../styles/Home.css';
-import Searchbar from './Searchbar';
+import JobSearch from './JobSearch';
 import Cards from './Cards';
 
 const Home = () => {
-    return (
-        <div className="home">
-            <div className="home-header">
-                <h1>İlanları Keşfet</h1>
-                <div className="search-box">
-                    <Searchbar />
-                </div>
-            </div>
-
-            <div className="all-jobs-section">
-                <h2>Tüm İlanlar</h2>
-                <div className="cards-container">
-                    {/* <CreateJob /> */}
-                    <Cards />
-                    {/* Burada tüm iş ilanlarını göstereceğiz */}
-                </div>
-            </div>
+  return (
+    <div className="home">
+      <div className="home-header">
+        <h1>İlanları Keşfet</h1>
+        <div className="search-box">
+          <JobSearch />
         </div>
-    );
+      </div>
+
+      <div className="all-jobs-section">
+        <h2>Tüm İlanlar</h2>
+        <div className="cards-container">
+          <Cards />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
