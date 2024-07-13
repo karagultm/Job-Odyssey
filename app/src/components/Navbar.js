1570693a2b0c4ab8bc5f17126e7bad53191cda2f
Muhammed Karagül <talhakaragul@marun.edu.tr>
2024-07-13 13:52:39 +0300

    page name changed

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -43,7 +43,7 @@ const Navbar = () => {
         </li>
         {userType !== "company" && (
           <li>
-            <Link to="/matched-jobs">Sana Özel İlanlar</Link>
+            <Link to="/matched-jobs">For You</Link>
           </li>
         )}
         {userType === "company" && (
@@ -68,7 +68,7 @@ const Navbar = () => {
         )}
         {userType === "applicant" && (
           <li>
-            <Link to="/my-profile">Profilim</Link>
+            <Link to="/my-profile">Profile</Link>
           </li>
         )}
       </ul>
